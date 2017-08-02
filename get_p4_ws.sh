3b52a22c26b7874f7859114c5a7fdd385dda46d2
Sandeep Varshney <svarshney@infinera.com>
2017-08-02 17:58:21 +0530

    some changes

--- get_p4_ws.sh
+++ get_p4_ws.sh
@@ -4,14 +4,45 @@ Branch=$2
 Host=`hostname`
 Host=`echo $host | sed 's/\.infinera.com//g'`
 Client=CLV_${Branch}_${Host}
+templatefile=templates
+tfile=""
+bfound=0
+
+if [ ! -f $templatefile ]; then
+   echo "Template file list missing"
+   exit 1
+fi
+
+function get_template {
+   while read line 
+   do 
+      b=`echo $line | awk '{print $1}'` 
+      t=`echo $line | awk '{print $2}'` 
+      if [ "$b" == "$Branch" ]; then    
+        tfile=$t
+      fi 
+    done < ${templatefile}
+    if [ ! $tfile ]; then
+       echo "Could not found build template for branch $Branch. Please make sure $templatefile is updated"
+       exit 1
+    fi
+}
 
 if [ -z ${Branch} ]; then
    echo "Branch Name is missing"
    exit 1
 fi
 
-p4 clients | grep $Client > /dev/null
-if [ $? -eq 0 ]; then
+get_template
+
+while read line
+do
+ if [ "$line" = "$Branch" ]; then
+    bfound=1
+ fi
+done< <(p4 clients | grep $Branch | awk '{print $2}')
+
+if [ $bfound -eq 1 ]; then
    echo "Client $Client is already here."
 else
    if [[ $host =~ in-* ]];
@@ -19,7 +50,7 @@ else
    elif [[ $host =~ sv-* ]];
       RootFolder="/home/bangbuild/CLVERI/workspace/${Client}/"
    fi
-   p4 -d $RootFolder -t ${Branch}_template -o ${Client} | p4 client -i
+   p4 -d $RootFolder -t $tfile -o ${Client} | p4 client -i
    if [ $? -ne 0 ];
       echo "Client ${Client} has not been created."
       exit 1
