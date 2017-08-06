6b661b3d384f428d85de186df252ebbb92860b2a
Sandeep Varshney <svarshney@infinera.com>
2017-08-06 17:35:46 +0530

    Change in tool

--- get_p4_ws.sh
+++ get_p4_ws.sh
@@ -1,58 +1,70 @@
-#!/usr/bin/env sh
+#!/usr/bin/env sh 
 
-Branch=$2
-Host=`hostname`
-Host=`echo $host | sed 's/\.infinera.com//g'`
+Branch=$1
+Host=`hostname | sed 's/\.infinera\.com//g'`
 Client=CLV_${Branch}_${Host}
 templatefile=templates
 tfile=""
 bfound=0
+alias p4="/usr/bin/p4 -u bangbuild -P BD09EFDFEEA034D237ADE61B256006A9"
+echo "Info: Host Name is $Host"
 
 if [ ! -f $templatefile ]; then
-   echo "Template file list missing"
+   echo "Error: Template file list missing"
    exit 1
 fi
 
 function get_template {
-   while read line 
-   do 
-      b=`echo $line | awk '{print $1}'` 
-      t=`echo $line | awk '{print $2}'` 
-      if [ "$b" == "$Branch" ]; then    
+   while read line
+   do
+      b=`echo $line | awk '{print $1}'`
+      t=`echo $line | awk '{print $2}'`
+      if [ "$b" == "$Branch" ]; then
         tfile=$t
-      fi 
+      fi
     done < ${templatefile}
     if [ ! $tfile ]; then
-       echo "Could not found build template for branch $Branch. Please make sure $templatefile is updated"
+       echo "Error: Could not found build template for branch $Branch. Please make sure $templatefile is updated"
        exit 1
+    else
+       echo "Info: Template is $tfile"
     fi
 }
 
 if [ -z ${Branch} ]; then
-   echo "Branch Name is missing"
+   echo "Error: Branch Name is missing"
    exit 1
 fi
 
+echo "Info: Client Name is $Client"
 get_template
 
-while read line
-do
- if [ "$line" = "$Branch" ]; then
-    bfound=1
- fi
-done< <(p4 clients | grep $Branch | awk '{print $2}')
+p4 clients | grep ${Client} | awk '{print $2}' > tempfile.txt
+if [ ! -z tempfile.txt ]; then
+   while read line
+   do
+      if [ "$line" = "$Client" ]; then
+         bfound=1
+      fi
+   done < tempfile.txt
+fi
+rm -f tempfile.txt
 
 if [ $bfound -eq 1 ]; then
-   echo "Client $Client is already here."
+   echo "Info: Client $Client is already here."
 else
-   if [[ $host =~ in-* ]];
-      RootFolder="/home/bangbuild/CLVERI/workspace/${Client}/"
-   elif [[ $host =~ sv-* ]];
-      RootFolder="/home/bangbuild/CLVERI/workspace/${Client}/"
+   if [[ $Host =~ in-* ]]; then
+      RootFolder="/home/bangbuild/CLVERI/workspace/"
+   elif [[ $Host =~ sv-* ]]; then
+      RootFolder="/bld_home/bangbuild/CLVERI/workspace/"
    fi
-   p4 -d $RootFolder -t $tfile -o ${Client} | p4 client -i
-   if [ $? -ne 0 ];
-      echo "Client ${Client} has not been created."
+   cd $RootFolder
+   p4 client -t $tfile -o ${Client} | p4 client -i
+   if [ $? -ne 0 ]; then
+      echo "Error: Client ${Client} has not been created. Looks like there are some issue."
       exit 1
+   else
+      echo "Client $Client has been created" 
    fi
 fi
+exit 0
